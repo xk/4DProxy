890ce4c83064dd84d7bec14d0d5b59a2411ad507
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2012-05-03 16:35:26 +0200

    add data:len parameter + function display () + fix timing

--- 4dproxy_2.js
+++ 4dproxy_2.js
@@ -15,7 +15,7 @@ if (ok) {
 }
 
 if (!ok) {
-  console.log("*******************\n* 4D PROXY SERVER *\n*******************\n\n2011-06-15 jorge@jorgechamorro.com\n\nEste programa sirve para dos cosas:\n- Una es mostrar el orden en que se producen las solicitudes y las respuestas entre un cliente 4D y un servidor 4D conectados por tcp/ip.\n- La otra es introducir una latencia (expresada entre milisegundos) a cada paquete que se envía y a cada paquete que se recibe.\n\nNecesita 4 parámetros para funcionar:\n\nlat:ms    -> los milisegundos de latencia que hay que añadir (por defecto 0)\nsip:ip    -> la IP del servidor 4D remoto.\nsp:puerto -> el puerto del servidor 4D remoto (por defecto 19813).\npp:puerto del proxy -> el puerto al que hay que conectarse para acceder a través del proxy.\n\nPor ejemplo, para arrancar este proxy en el puerto 12345 de este ordenador, y hacer que se conecte a un servidor 4D que esté en la IP 10.0.0.1 en el puerto 19813, añadiendo una latencia de 50 ms habrá que teclear:\n\nnode 4dproxy.js pp:12345 sip:10.0.0.1 sp:19813 lat:50\n\nEl orden de los parámetros no importa.\nSi no se especifica lat: se supone que es 0.\nSi no se especifica sp: se supone que es 19813\n\n");
+  console.log("*******************\n* 4D PROXY SERVER *\n*******************\n\n2011-06-15 jorge@jorgechamorro.com\n\nEste programa sirve para dos cosas:\n- Una es mostrar el orden en que se producen las solicitudes y las respuestas entre un cliente 4D y un servidor 4D conectados por tcp/ip.\n- La otra es introducir una latencia (expresada entre milisegundos) a cada paquete que se envía y a cada paquete que se recibe.\n\nNecesita 4 parámetros para funcionar:\n\nlat:ms    -> los milisegundos de latencia que hay que añadir (por defecto 0)\nsip:ip    -> la IP del servidor 4D remoto.\nsp:puerto -> el puerto del servidor 4D remoto (por defecto 19813).\npp:puerto del proxy -> el puerto al que hay que conectarse para acceder a través del proxy.\ndata:len -> el número de caracteres que se quiere mostrar en data: (por defecto todos)\n\nPor ejemplo, para arrancar este proxy en el puerto 12345 de este ordenador, y hacer que se conecte a un servidor 4D que esté en la IP 10.0.0.1 en el puerto 19813, añadiendo una latencia de 50 ms habrá que teclear:\n\nnode 4dproxy.js pp:12345 sip:10.0.0.1 sp:19813 lat:50\n\nEl orden de los parámetros no importa.\nSi no se especifica lat: se supone que es 0.\nSi no se especifica sp: se supone que es 19813\n\n");
   return;
 }
 
@@ -25,6 +25,7 @@ var lat= (params.lat= (parseInt(params.lat, 10) || 0));
 var sp= (params.sp= (parseInt(params.sp, 10) || 19813));
 var sip= (params.sip= (params.sip || '0.0.0.0'));
 var pp= (params.pp= (parseInt(params.pp, 10) || 0));
+var datalen= (params.data= (parseInt(params.data, 10) || 0));
 console.log(params);
 
 var offset= sp- pp;
@@ -62,9 +63,7 @@ function clientConnection (clientSocket) {
     // Lo que nos llega del server lo metemos en un buffer para enviarlo al cliente.
     clientBuffer.push({data:data, t:Date.now()+lat});
     if (!writer.timer) writer.timer= setTimeout(writer, lat);
-    
-    var str= data.toString('ascii').replace(/\W/g, ".");
-    console.log('i '+ '['+ numero+ '] t:'+ ((Date.now()-time_0)/1000).toFixed(1)+ 's #'+ (ctr++)+ ' L:'+ data.length+ ' data:'+ str);
+    display('i', data);
   });
   
   
@@ -73,9 +72,7 @@ function clientConnection (clientSocket) {
     // Lo que nos llega del cliente lo metemos en un buffer para enviarlo al server.
     serverBuffer.push({data:data, t:Date.now()+lat});
     if (!writer.timer) writer.timer= setTimeout(writer, lat);
-    
-    var str= data.toString('ascii').replace(/\W/g, ".");
-    console.log('o '+ '['+ numero+ '] t:'+ ((Date.now()-time_0)/1000).toFixed(1)+ 's #'+ (ctr++)+ ' L:'+ data.length+ ' data:'+ str);
+    display('o', data);
   });
   
   clientSocket.on('end', function () {
@@ -86,15 +83,28 @@ function clientConnection (clientSocket) {
   });
   
   
+  function display (prefijo, data) {
+    var str;
+    if (datalen) {
+      str= data.toString('ascii', 0, datalen);
+    }
+    else {
+      str= data.toString('ascii');
+    }
+    str= str.replace(/\W/g, ".");
+    console.log(prefijo+ ' ['+ numero+ '] t:'+ ((Date.now()-time_0)/1000).toFixed(1)+ 's #'+ (ctr++)+ ' L:'+ data.length+ ' data:'+ str);
+  }
+  
+  
   //writer() envía a sus destinos el contenido de los buffers.
   function writer () {
-    var t1= 0, t2= 0, now;
+    var t= 0;
+    var now= Date.now();
     
     if (serverSocket) {
       while (serverBuffer.length) {
-        now= Date.now();
         if (serverBuffer[0].t > now) {
-          t2= serverBuffer[0].t- now;
+          t= serverBuffer[0].t;
           break;
         }
         serverSocket.write(serverBuffer.shift().data);
@@ -103,16 +113,30 @@ function clientConnection (clientSocket) {
     
     if (clientSocket) {
       while (clientBuffer.length) {
-        now= Date.now();
         if  (clientBuffer[0].t > now) {
-          t1= clientBuffer[0].t- now;
+          t= t ? Math.min(t, clientBuffer[0].t) : clientBuffer[0].t;
           break;
         }
         clientSocket.write(clientBuffer.shift().data);
       }
     }
     
-    writer.timer= (t1 || t2) ? setTimeout(writer, Math.min(t1, t2)) : 0;
+    if (t) {
+      t-= Date.now();
+      t= (t > 0) ? t : 0;
+      if (t) {
+        writer.timer= setTimeout(writer, t);
+        console.log('*************************************************** setTimeout(writer, '+ t+ ')');
+      }
+      else {
+        process.nextTick(writer);
+        writer.timer= 1;
+        console.log('*************************************************** process.nextTick(writer)');
+      }
+    }
+    else {
+      writer.timer= 0;
+    }
   }
 }
 
